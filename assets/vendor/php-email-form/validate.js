175b5f005d965587ec6656a365e8d385f2ace7fc
andres <aaburgos@smdigital.com.co>
2020-05-03 09:33:39 -0500

    formaulario

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -97,7 +97,7 @@ jQuery(document).ready(function($) {
 
     if( ! action ) {
       this_form.find('.loading').slideUp();
-      this_form.find('.error-message').slideDown().html('The form action property is not set!');
+      this_form.find('.error-message').slideDown().html('¡La propiedad de acción de formulario no está establecida!');
       return false;
     }
     
